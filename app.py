ef3c32a5a420989a34587bdd373daa19b973957f
flourishenebe-del <flourishenebe@gmail.com>
2026-01-08 11:43:53 -0800

    Resolved merge conflict

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 print('Hello Git')
-print('user login feature)
+print('user login feature')
 print('Added Dashboard UI')
-<<<<<<< HEAD
 print('Hello from B')
-=======
 print('Hello from A')
->>>>>>> 74861fab7d039cf59539d2fcd206aa1256b68215
